e3891b80c5b2757e890ad42b17867cfda9a086c8
furquantekisky <153543573+furquantekisky@users.noreply.github.com>
2023-12-25 18:06:00 +0530

    update

---
+++ mmhAPI/controllers/dashboardController.js
@@ -0,0 +1,90 @@
+import PatientModel from '../models/patientModel.js'
+import {
+getTotalAmountService,
+getThisMonthTotalAmountService,
+totalNumberOfCaseCloseService,
+totalNumberOfCaseCloseInMonthService,
+totalNumberOfApproachService, totalNumberOfMonthApproachService,
+findPendingCasesMoreThan5DaysService,
+operatorDetailsBasedOnStatusService
+} from '../services/dashboardService.js'
+
+export const dashboardController = async (req, res) => {
+
+    try {
+        const totalAmountSaved = await getTotalAmountService()
+        // console.log(" Total  Amount saved ===>" ,totalAmountSaved);
+
+        const monthAmountSaved = await getThisMonthTotalAmountService()
+        // console.log( " Total month Amount Saved ===>" ,monthAmountSaved);
+
+
+        const totalClosedCases = await totalNumberOfCaseCloseService()
+        // console.log(totalClosedCases);
+
+
+        const totalClosedCasesInMonth = await totalNumberOfCaseCloseInMonthService()
+        // console.log("totalClosedCasesInMonth ===>" , totalClosedCasesInMonth);
+
+
+        const totalNumberOfApproach = await totalNumberOfApproachService()
+        // console.log("approach ===>",totalNumberOfApproach);
+
+
+        const totalNumberOfMonthApproach = await totalNumberOfMonthApproachService()
+        // console.log("month ===>",totalNumberOfMonthApproach);
+
+
+        const PendingCasesMoreThan5Days = await findPendingCasesMoreThan5DaysService()
+        // console.log("findPendingCasesMoreThan5DaysService==>" , findPendingCasesMoreThan5Days);
+
+        res.status(200).json({
+            success: true,
+            totalAmountSaved: totalAmountSaved,
+            monthAmountSaved: monthAmountSaved,
+            totalClosedCases: totalClosedCases,
+            totalClosedCasesInMonth: totalClosedCasesInMonth,
+            totalNumberOfApproach: totalNumberOfApproach,
+            totalNumberOfMonthApproach: totalNumberOfMonthApproach,
+            PendingCasesMoreThan5Days: PendingCasesMoreThan5Days
+
+        })
+
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Error While Getting the result",
+            error: error.message
+        })
+    }
+    // res.status()
+
+}
+
+
+
+export const dashboardOperatorDetailsController = async (req, res) => {
+
+    try {
+         const {phoneNumber} = req.query
+         console.log( "number ==> cont " ,phoneNumber);
+         const details = await operatorDetailsBasedOnStatusService(phoneNumber)
+
+         res.status(200).json({
+            success: true,
+            details
+         })
+
+        } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Error While Getting the result",
+            error: error.message
+        })
+    }
+
+
+    // http://localhost:4000/mmh/dashboard/operator?createdBy=8888888888
+    // http://localhost:4000/mmh/dashboard/operator?createdBy=8888888888&status=Closed-Civil
+}
